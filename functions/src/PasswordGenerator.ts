85df122714956dd81ee12609ba3833f17b0dc0eb
Daichi Hioki <hiokidaichi@gmail.com>
2019-05-16 12:50:39 +0900

    Generate random 16 alphabet password

--- functions/src/PasswordGenerator.ts
+++ functions/src/PasswordGenerator.ts
@@ -1,7 +1,14 @@
+const chars = "abcdefghijklmnopqrstuvwxyz";
+const passwordLength = 16;
+
 export default class PasswordGenerator {
   constructor() {}
 
   generate(): string {
-    return "password";
+    const charsLength = chars.length;
+
+    return [...Array(passwordLength)]
+      .map(() => chars[Math.floor(Math.random() * charsLength)])
+      .join("");
   }
 }
